a927eccfa4e3204d60c0b68fc208d36109e6c31a
Numa52 <77755196+Numa52@users.noreply.github.com>
2021-01-31 23:41:36 -0600

    Completed code for HW1

--- Homework1/Problem1Code.py
+++ Homework1/Problem1Code.py
@@ -1,3 +1,17 @@
-current_date = input("Please insert the current date(mm/dd/yy): ")
-print(current_date)
-#test
+print("This is a birthday Calculator")
+#current day inputs
+m1 = int(input("Please enter the current month(1-12): "))
+d1 = int(input("Please enter the current day(1-31): "))
+y1 = int(input("Please enter the current year: "))
+#users birthday date input
+print("Please enter your birthday date.")
+m2 = int(input("Please enter your birth month(1-12): "))
+d2 = int(input("Please enter your birth day(1-31): "))
+y2 = int(input("Please enter your birth year: "))
+
+years = y1-y2-1
+
+if (m1==m2 and d1==d2):
+    print("Happy birthday!")
+
+print("You are", years, "years old.")
