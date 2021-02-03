8c3bf053cebaa0e1bf11638fc5c3c53b2a852c79
Numa52 <77755196+Numa52@users.noreply.github.com>
2021-02-03 17:42:15 -0600

    Added comments for coding problem1 that explains the lines

--- Homework1/Problem1Code.py
+++ Homework1/Problem1Code.py
@@ -9,8 +9,9 @@ m2 = int(input("Please enter your birth month(1-12): "))
 d2 = int(input("Please enter your birth day(1-31): "))
 y2 = int(input("Please enter your birth year: "))
 
+#Equation that caluclates the user's age
 years = y1-y2-1
-
+#If statement that determines if the users birthday is the current date
 if (m1==m2 and d1==d2):
     print("Happy birthday!")
 
