04528ff7a9bef5726b3a58fae3781872b836308e
Numa52 <77755196+Numa52@users.noreply.github.com>
2021-02-04 22:46:04 -0600

    Problem 1 code done 1.12 lab done 1.20 lab done

---
+++ Homework1/1.20.py
@@ -0,0 +1,20 @@
+#Ryan Nguyen PSID: 1805277
+
+#getting input from user
+user_num1 = int(input("Enter integer:\n"))
+num1sqrd = user_num1 * user_num1
+num1cbed = user_num1 * user_num1 * user_num1
+
+#print num1
+print("You entered:", user_num1)
+print(user_num1,"squared is", num1sqrd)
+print("And", user_num1, "cubed is", num1cbed, "!!")
+
+#getting input 2 from user
+user_num2 = int(input("Enter another integer:\n"))
+num1_num2_sum = user_num1 + user_num2
+num1_num2_product = user_num1 * user_num2
+
+#print num2
+print(user_num1, "+", user_num2, "is", num1_num2_sum)
+print(user_num1, "*", user_num2, "is", num1_num2_product)
